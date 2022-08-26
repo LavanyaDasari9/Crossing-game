168ab4b3e5174d3f017a8e800d46dd8dca643d05
DASARI LAVANYA <99727962+LavanyaDasari9@users.noreply.github.com>
2022-08-26 16:26:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("crossing game")
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_cars()
+    car_manager.move()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+        if player.finish():
+            player.go_to_start()
+            car_manager.level_up()
+
+
+screen.exitonclick()
